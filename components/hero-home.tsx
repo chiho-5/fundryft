97aadb102ce99b7c2798f1a114a7267a1e8d6bc9
Ezeora Nnenna Esther <71467633+chiho-5@users.noreply.github.com>
2024-11-12 00:46:42 +0100

    Update hero-home.tsx

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -26,7 +26,7 @@ export default function HeroHome() {
   // };
 
   return (
-    <section>
+    <section className="bg-gradient-to-t from-indigo-500 via-purple-600 to-indigo-800">
       <div className="mx-auto max-w-6xl px-4 sm:px-6">
         <div className="py-12 md:py-20">
           <div className="pb-12 text-center md:pb-20">
@@ -58,7 +58,7 @@ export default function HeroHome() {
                     </span>
                   </a>
                 </div>
-{/*                 <div data-aos="fade-up" data-aos-delay={600}>
+                {/* <div data-aos="fade-up" data-aos-delay={600}>
                   <button
                     className="btn relative w-full bg-gradient-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] text-gray-300 hover:bg-[length:100%_150%] sm:ml-4 sm:w-auto"
                     onClick={handleScheduleCall}
